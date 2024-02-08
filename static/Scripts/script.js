6877fcd4030f249f289f0a90d2d40d08c0e52595
Mahmoud Sabry <mahmoudsabry.udacity@gmail.com>
2024-02-08 18:45:06 +0200

    dddd

--- static/Scripts/script.js
+++ static/Scripts/script.js
@@ -1,7 +1,194 @@
 
+const transilations = {
+  en:{
 
+    //NAV
+    // home:"Home",
+    // detector:"Detector",
+    // ecg:"Ecg",
+    // stroke:"Stroke",
+    // chd:"Chd",
+    // analysis:"Analysis",
+    // strokeAnalysis:"Stroke",
+    // chdAnalysis:"Chd",
+    // about:"About",
+    // chatbot:"Chatbot",
+    english:"English",
+    arabic:"Arabic",
 
+    // STROKE
+    STtitle:"Stroke Questionaire",
+    STtitle2:"Answer this Questions",
+    strokeTitle:"What is your gender?",
+    gender:"Gender",
+    male:"Male",
+    female:"Female",
+    currentWork:"Which of the following best describes your current work status?",
+    private:"Private",
+    govtJob:"Govt_job",
+    children:"Children",
+    age:"What is your current age?",
+    bloodPressure:"Have you been diagnosed with hypertension (high blood pressure)?",
+    yse:"Yes",
+    no:"No",
+    status:"Have you ever been married?",
+    heartDisease:"Do you have Heart disease",
+    live:"Do you live in an urban or rural area?",
+    urban:"Urban",
+    rural:"Rural",
+    glucoseLevel:"What is your most recent fasting blood glucose level (mg/dL)?",
+    bodyMass:"What is your current Body Mass Index (BMI)?",
+    smokeStatus:"Which of the following best describes your smoking status?",
+    neverSmoke:"Never Smoked",
+    smoke:"Smokes",
+    formerlySmoke:"Formerly Smoked",
 
+    // ECG
+    ECGtitle:"Our Services",
+    ECGtitle2:"Tell Us How Can Help?",
+    ECGhead:"Part 1: Quick Questionaire",
+    ckmb:"CK-MB",
+    troponin:"Troponin",
+
+    //CHD
+    CHDtitle:"CHD Questionaire",
+    CHDtitle2:"Answer this  Questions",
+    cigsNum:"If you currently smoke, how many cigarettes do you smoke per day?",
+    troponibloodPressuren:"TroponAre you currently taking any blood pressure medications?in",
+    stroke:"Have you ever had a stroke?",
+    hypertension:"Have you ever been diagnosed with hypertension (high blood pressure)?",
+    diabetes:"Have you ever been diagnosed with diabetes?",
+    cholesterolLevel:"What is your most recent total cholesterol level (mg/dL)?",
+    topBloodPressure:"What is your most recent systolic blood pressure reading (the top number)?",
+    bottomBloodPressure:"What is your most recent diastolic blood pressure reading (the bottom number)?",
+    bmi:"What is your current body mass index (BMI)",
+    restingHeartRate:"What is your resting heart rate (beats per minute)?",
+    glucoseLevel:"What is your most recent fasting blood glucose level (mg/dL)?",
+
+    //RESULT
+    formCheck:"Check your Heart",
+    result:"Your Result",
+
+    // DOCTOR
+    // meetDr:"Meet Our",
+    // topDr:"Top Rated Doctors",
+    // topicsDr:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Quasi, voluptas exercitationem atque mollitia necessitatibussuscipit laudantium quia incidunt molestias dolorum! Quasiincidunt doloremque quisquam iusto?Dolore necessitatibus eligendi voluptates laudantium!",
+    // drName:"Smokes",
+    // drLoc:"Smokes",
+    // drPrice:"Smokes",
+    // drBook:"Book Appointment",
+
+    // detector:"Detector",
+
+  },
+  ar:{
+    // home:"الرئيسية",
+    // detector:"الكشف",
+    // ecg:"Ecg",
+    // stroke:"Stroke",
+    // chd:"Chd",
+    // analysis:"تحاليل",
+    // strokeAnalysis:"Stroke",
+    // chdAnalysis:"Chd",
+    // about:"عنا",
+    // chatbot:"الدردشة",
+    english:"انجليزي",
+    arabic:"عربي",
+    STtitle:"استبيان Stroke ",
+    STtitle2:"املا هذه البيانات",
+    gender:"ما هو نوعك",
+    male:"ذكر",
+    female:"أنثي",
+    currentWork:" أي مما يلي يصف بشكل أفضل حالة عملك الحالية ؟",
+    private:"خاص",
+    govtJob:"وظائف حكومية",
+    children:"أطفال",
+    age:"ما هو عمرك الحالي",
+    bloodPressure:"هل تم تشخيص إصابتك بارتفاع ضغط الدم (ارتفاع ضغط الدم)؟",
+    yes:"نعم",
+    no:"لا",
+    status:"هل سبق لك الزواج؟",
+    heartDisease:"هل تعاني من مرض القلب",
+    live:"هل تعيش في منطقة حضرية أم ريفية؟",
+    urban:"حضري",
+    rural:"ريفي",
+    glucoseLevel:"ما هو أحدث مستوى للجلوكوز في الدم أثناء الصيام (مجم/ديسيلتر)؟",
+    bodyMass:"ما هو مؤشر كتلة جسمك الحالي (BMI)؟",
+    smokeStatus:"أي مما يلي يصف حالة التدخين لديك بشكل أفضل؟",
+    neverSmoke:"لم ادخن أبدا",
+    smoke:"ادخن",
+    formerlySmoke:"مدخن سابقا",
+    formCheck:"ابدا الفحص",
+
+    //ECG
+    ECGtitle:"ترجم",
+    ECGtitle2:"ترجم",
+    ECGhead:"ترجم",
+    ckmb:"ترجم",
+    troponin:"ترجم",
+
+     //CHD
+     CHDtitle:"ترجم",
+     CHDtitle2:"ترجم",
+     cigsNum:"ترجم",
+     troponibloodPressuren:"ترجم",
+     stroke:"ترجم",
+     hypertension:"ترجم",
+     diabetes:"ترجم",
+     cholesterolLevel:"ترجم",
+     topBloodPressure:"ترجم",
+     bottomBloodPressure:"ترجم",
+     bmi:"ترجم",
+     restingHeartRate:"ترجم",
+     glucoseLevel:"ترجم",
+ 
+    //RESULT
+    result:"نتيجتك",
+
+    //DOCTOR
+    // meetDr:"تواصل مع",
+    // topDr:"الاطباء الاعلي تقييما",
+    // topicsDr:"Lorem ipsum dolor sit amet consectetur adipisicing elit.Quasi, voluptas exercitationem atque mollitia necessitatibussuscipit laudantium quia incidunt molestias dolorum! Quasiincidunt doloremque quisquam iusto?Dolore necessitatibus eligendi voluptates laudantium!",
+    // drName:"Smokes",
+    // drLoc:"Smokes",
+    // drPrice:"Smokes",
+    // drBook:"Book Appointment",
+    // detector:"Detector",
+
+  },
+}
+
+const langSelector = document.querySelector("select")
+
+langSelector.addEventListener("change",(event)=>{
+  setLanguage(event.target.value)
+  // console.log(event.target.value)
+  // localStorage.setItem("lang",event.target.value)
+  // langSelector.options.removeAttribute("selected")
+})
+document.addEventListener('DOMContentLoaded',()=>{
+  // setLanguage(localStorage.getItem("lang"))
+  // langSelector.textContent=localStorage.getItem("lang")
+})
+
+const setLanguage = (language)=>{
+  const elements = document.querySelectorAll('[data-lng]')
+  // console.log(element)
+  elements.forEach((element)=>{
+    const transilationKey = element.getAttribute("data-lng")
+    element.textContent = transilations[language][transilationKey]
+  })
+
+  // if(lang === 'ar'){
+  //   document.getElementById('my-form').dir= 'rtl'
+  //   document.getElementById("formBtn").style.marginRight='13rem'
+  //   document.getElementById('Services-Sheader').dir= 'rtl'
+  // }
+  document.getElementById('Services-Sheader').dir = language === 'ar'? 'rtl': 'ltr';
+  document.getElementById('my-form').dir = language === 'ar'? 'rtl': 'ltr'
+  document.getElementById("formBtn").style.marginRight = language==='ar'?'13rem':'0'
+
+}
 
 
 
@@ -48,7 +235,7 @@ window.onscroll = function() {
 
 
 document.getElementById("homeBtn").addEventListener("click", function() {
-  document.getElementById("Services").scrollIntoView({ behavior: "smooth" });
+  document.getElementById("Services-Sheader").scrollIntoView({ behavior: "smooth" });
 });
 
 // window.addEventListener("scroll", function() {
@@ -136,6 +323,7 @@ setTimeout(() => {
 //     document.getElementById("Navebar").style.left = "-50px";
 //   }
 // }
+
 
 
 
